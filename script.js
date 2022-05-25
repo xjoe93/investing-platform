21208a6d3b6eb1ec828500b717c4c19c973a3046
unknown <xjoestar93@gmail.com>
2022-05-25 11:46:11 +0200

    Hello Platform

---
+++ script.js
@@ -0,0 +1,9 @@
+let listShow = document.getElementById("show-list");
+
+function showList() {
+    if (listShow.style.display === "none") {
+        listShow.style.display = "block";
+    } else {
+        listShow.style.display = "none";
+    }
+}
